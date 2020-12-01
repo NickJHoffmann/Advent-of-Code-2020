79d9e6a337653f83ddd48ecbcada763053783f08
BuildTools <unconfigured@null.spigotmc.org>
2020-12-01 15:04:52 -0500

    Day 1

---
+++ day1.py
@@ -0,0 +1,26 @@
+with open("day1.txt") as file:
+    data = file.read().splitlines()
+    for i in range(len(data)):
+        data[i] = int(data[i])
+    numToCheck = int(input("Enter number of entries to combine: "))
+    num1 = 0
+    num2 = 0
+    num3 = 0    #For part 2
+    if numToCheck == 2:
+        for entry in data:
+            temp = 2020-entry
+            if temp in data:
+                num1 = entry
+                num2 = temp
+                break
+        print(num1 * num2)
+    elif numToCheck == 3:
+        try:
+            for entry1 in data:
+                for entry2 in data:
+                    check = 2020 - (entry1 + entry2)
+                    if check in data:
+                        print(entry1 * entry2 * check)
+                        raise Exception("Done")
+        except:
+            pass
