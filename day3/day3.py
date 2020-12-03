401d7bc25ffb84744e7553676f3d0d1813ba3de3
BuildTools <unconfigured@null.spigotmc.org>
2020-12-03 17:38:45 -0500

    Reduce amount of I/O required

--- day3/day3.py
+++ day3/day3.py
@@ -1,16 +1,13 @@
 filename = "day3.txt"
 
 # Determines the amount of trees intersected using a given slope
-def findTrees(xSlope, ySlope):
-    with open(filename, 'r') as file:
-        mapRows = file.readlines()
-
-    lineLength = len(mapRows[0])   # Max length of each line, stored for readability later
+def findTrees(inputData, xSlope, ySlope):
+    lineLength = len(inputData[0])   # Max length of each line, stored for readability later
     treeCount = 0
     xPos = 0              # Current horizontal position
     currentLine = 0
-    while currentLine < len(mapRows):
-        row = mapRows[currentLine].strip()
+    while currentLine < len(inputData):
+        row = inputData[currentLine].strip()
         if row[xPos] == '#':
             treeCount += 1
         xPos += xSlope
@@ -22,13 +19,17 @@ def findTrees(xSlope, ySlope):
         currentLine += ySlope
     return treeCount
 
+# Load data to reduce I/O time
+with open(filename, 'r') as file:
+    mapRows = file.readlines()
+
 # Part 1
-part1 = findTrees(3, 1)
+part1 = findTrees(mapRows, 3, 1)
 print("Answer for Part 1: {}".format(part1))
 
 # Part 2
 slopes = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]   # Slopes given in Part 2, listed in (x, y) notation
 numOfTrees = 1          # Need to multiply all answers, so initialize to 1 to allow me to use *= on each value
 for xSlope, ySlope in slopes:
-    numOfTrees *= findTrees(xSlope, ySlope)
+    numOfTrees *= findTrees(mapRows, xSlope, ySlope)
 print("Answer for Part 2: {}".format(numOfTrees))
