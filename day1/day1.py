682e327a5e5c4262698741122ac94a3f0175b07f
BuildTools <unconfigured@null.spigotmc.org>
2020-12-02 22:31:49 -0500

    Small formatting cleanup

--- day1/day1.py
+++ day1/day1.py
@@ -1,26 +1,24 @@
 with open("day1.txt") as file:
     data = file.read().splitlines()
-    for i in range(len(data)):
-        data[i] = int(data[i])
-    numToCheck = int(input("Enter number of entries to combine: "))
-    num1 = 0
-    num2 = 0
-    num3 = 0
-    if numToCheck == 2:
-        for entry in data:
-            temp = 2020-entry
-            if temp in data:
-                num1 = entry
-                num2 = temp
-                break
-        print(num1 * num2)
-    elif numToCheck == 3:
-        try:
-            for entry1 in data:
-                for entry2 in data:
-                    check = 2020 - (entry1 + entry2)
-                    if check in data:
-                        print(entry1 * entry2 * check)
-                        raise Exception("Done")
-        except:
-            pass
+
+for i in range(len(data)):
+    data[i] = int(data[i])
+num1 = 0
+num2 = 0
+num3 = 0
+for entry in data:
+    temp = 2020 - entry
+    if temp in data:
+        num1 = entry
+        num2 = temp
+        break
+print("Answer to Part 1: {}".format(num1 * num2))
+try:
+    for entry1 in data:
+        for entry2 in data:
+            check = 2020 - (entry1 + entry2)
+            if check in data:
+                print("Answer to Part 2: {}".format(entry1 * entry2 * check))
+                raise Exception("Done")
+except:
+    pass
--- day2/day2.py
+++ day2/day2.py
@@ -1,23 +1,24 @@
 with open("day2.txt", 'r') as file:
     line = file.readline()
-    validP1 = 0
-    validP2 = 0
+    p1Count = 0
+    p2Count = 0
     while line:
         data = line.split()
         nums = data[0].split('-')
-        num1 = int(nums[0])
-        num2 = int(nums[1])
+        lowerBound = int(nums[0])
+        upperBound = int(nums[1])
         letter = data[1].replace(':', "")
         password = data[2]
 
         #Part 1
-        occur = password.count(letter)
-        if num1 <= occur <= num2:
-            validP1 += 1
+        occurances = password.count(letter)
+        if lowerBound <= occurances <= upperBound:
+            p1Count += 1
 
         #Part 2
-        if (password[num1-1] == letter and not password[num2-1] == letter) or (not password[num1-1] == letter and password[num2-1] == letter):
-            validP2 += 1
+        if bool(password[lowerBound - 1] == letter) ^ bool(password[upperBound - 1] == letter):
+            p2Count += 1
+
         line = file.readline()
-    print(validP1)
-    print(validP2)
+    print("Answer to Part 1: {}".format(p1Count))
+    print("Answer to Part 2: {}".format(p2Count))
