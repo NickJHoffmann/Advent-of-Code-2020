e26b72416962b17d643f04dda818705f09fddc3d
BuildTools <unconfigured@null.spigotmc.org>
2020-12-03 17:32:14 -0500

    Fixed formatting, renamed variables

--- day3/day3.py
+++ day3/day3.py
@@ -7,17 +7,17 @@ def findTrees(xSlope, ySlope):
 
     lineLength = len(mapRows[0])   # Max length of each line, stored for readability later
     treeCount = 0
-    currentPos = 0              # Current horizontal position
+    xPos = 0              # Current horizontal position
     currentLine = 0
     while currentLine < len(mapRows):
-        row = mapRows[currentLine]
-        if row[currentPos] == '#':
+        row = mapRows[currentLine].strip()
+        if row[xPos] == '#':
             treeCount += 1
-        currentPos += xSlope
+        xPos += xSlope
 
         # If pointer is at the end of line, calculate "overflow" to determine what x position to start next line
-        if currentPos >= lineLength-1:
-            currentPos = currentPos - lineLength + 1
+        if xPos >= lineLength-1:
+            xPos = xPos - lineLength + 1
 
         currentLine += ySlope
     return treeCount
@@ -29,6 +29,6 @@ print("Answer for Part 1: {}".format(part1))
 # Part 2
 slopes = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]   # Slopes given in Part 2, listed in (x, y) notation
 numOfTrees = 1          # Need to multiply all answers, so initialize to 1 to allow me to use *= on each value
-for slope in slopes:
-    numOfTrees *= findTrees(slope[0], slope[1])
+for xSlope, ySlope in slopes:
+    numOfTrees *= findTrees(xSlope, ySlope)
 print("Answer for Part 2: {}".format(numOfTrees))
