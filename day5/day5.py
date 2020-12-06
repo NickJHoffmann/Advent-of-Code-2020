8d786ad699840a4fcc8c7d578bc13bc0ed7a07e1
BuildTools <unconfigured@null.spigotmc.org>
2020-12-05 19:19:50 -0500

    Removed unnecessary variables

--- day5/day5.py
+++ day5/day5.py
@@ -8,30 +8,27 @@ with open(filename, 'r') as file:
 # Calculate seat id of a given boarding pass
 def getSeats(data):
     line = data.strip()
+
+    # Establish row and column bounds based on given prompt
     rowLower = 0
     rowUpper = 127
-    rowFinal = 0
 
     colLower = 0
     colUpper = 7
-    colFinal = 0
+
     for char in line:
         # Change the bounds of the current partition depending on what character is encountered
         if char == "B":
             rowLower += math.ceil((rowUpper-rowLower)/2)
-            rowFinal = min(rowLower, rowUpper)
         elif char == "F":
             rowUpper -= math.ceil((rowUpper-rowLower)/2)
-            rowFinal = max(rowLower, rowUpper)
         elif char == "L":
             colUpper -= math.ceil((colUpper-colLower)/2)
-            colFinal = min(colLower, colUpper)
         elif char == "R":
             colLower += math.ceil((colUpper-colLower)/2)
-            colFinal = max(colLower, colUpper)
         else:
             print("Invalid character in boarding pass")
-    seatID = rowFinal * 8 + colFinal    # Given formula to calculate seat ID
+    seatID = rowLower * 8 + colLower    # Given formula to calculate seat ID
     return seatID
 
 
