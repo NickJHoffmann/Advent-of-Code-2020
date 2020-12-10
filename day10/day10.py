555ff45ec108e964b61710011a0f16b579e1cff0
BuildTools <unconfigured@null.spigotmc.org>
2020-12-10 15:41:54 -0500

    Part 2 working

--- day10/day10.py
+++ day10/day10.py
@@ -1,4 +1,4 @@
-filename = "example.txt"
+filename = "day10.txt"
 
 data = []
 with open(filename, 'r') as file:
@@ -29,36 +29,19 @@ setData = set(data)
 
 
 def find_paths(index):
-    # Incorrect: 11239424 (too low)
-
     paths = 0
     start = data[index]
     if start == data[-1]:
         return 1
+    if start in completePaths:
+        return completePaths[start]
     if start + 1 in setData:
-        print("found start + 1:", start + 1)
-        try:
-            paths += completePaths[start+1]
-        except Exception as e:
-            found = find_paths(index+1)
-            completePaths[start+1] = found
-            paths += found
+        paths += find_paths(data.index(start + 1))
     if start + 2 in setData:
-        print("found start + 2:", start + 2)
-        try:
-            paths += completePaths[start+2]
-        except Exception as e:
-            found = find_paths(index+2)
-            completePaths[start+2] = found
-            paths += found
+        paths += find_paths(data.index(start + 2))
     if start + 3 in setData:
-        print("found start + 3:", start + 3)
-        try:
-            paths += completePaths[start+3]
-        except Exception as e:
-            found = find_paths(index+3)
-            completePaths[start+3] = found
-            paths += found
+        paths += find_paths(data.index(start + 3))
+    completePaths[start] = paths
     return paths
 
 
